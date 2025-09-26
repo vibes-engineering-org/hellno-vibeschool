7e75df28a16a0b61355e621496de81a0cfe113e9
vibesengineering <205912982+vibesengineering@users.noreply.github.com>
2025-09-26 11:36:07 +0000

    Code generation: 3a216ab7

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -6,6 +6,13 @@ export default function App() {
   return (
     <div className="w-[400px] mx-auto py-8 px-4 min-h-screen flex flex-col items-center justify-center">
       {/* TEMPLATE_CONTENT_START - Replace content below */}
+      <div className="mb-8 text-center">
+        <img
+          src="/logo_small.png"
+          alt="Logo"
+          className="mx-auto mb-4 h-12 w-auto"
+        />
+      </div>
       <WaitlistForm />
       {/* TEMPLATE_CONTENT_END */}
     </div>
